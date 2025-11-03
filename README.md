1f86d351f33dc59eaa3d77932db5e4a5c7ccfaf1
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2025-11-03 16:22:43 -0500

    ajustes de seeders y readme

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-"# prueba_django" 
+# prueba_django
+
+1. Instalar virtualenv (opcional)
+
+```cmd
+pip install virtualenv
+```
+
+2. Crear el entorno virtual
+
+```cmd
+python -m venv venv
+```
+
+3. Activar el entorno virtual
+
+```cmd
+venv\Scripts\activate
+```
+
+4. leer e instalar las dependencias
+
+```cmd
+cd prueba_django
+```
+
+```cmd
+pip install -r requirements.txt
+```
+
+5. Crear migraciones (a partir de tus modelos)
+
+```cmd
+python manage.py makemigrations
+```
+
+6. Aplicar las migraciones a la base de datos
+
+```cmd
+python manage.py migrate
+```
+
+7. Creacion de usuario de Pruebas
+
+```cmd
+python manage.py seedadmin
+```
+
+8. Creacion de Productos de Inventario
+
+```cmd
+python manage.py shell < inventario/seedproducto.py
+```
+
+9. Iniciar proyecto creado
+
+```cmd
+python manage.py runserver
+```
--- prueba_django/inventario/seedproducto.py
+++ prueba_django/inventario/seedproducto.py
@@ -9,14 +9,6 @@ productos = [
     {"nombre": "USB 32GB", "descripcion": "Memoria USB 32GB", "precio": 25.00 },
     {"nombre": "Webcam", "descripcion": "Webcam HD", "precio": 80.00 },
     {"nombre": "Silla Gamer", "descripcion": "Silla ergonómica", "precio": 600.00 },
-    """ {"nombre": "Mouse", "descripcion": "Mouse óptico", "precio": 50.00, "stock": 20},
-    {"nombre": "Teclado", "descripcion": "Teclado mecánico", "precio": 180.00, "stock": 10},
-    {"nombre": "Monitor", "descripcion": "Monitor 24 pulgadas", "precio": 900.00, "stock": 5},
-    {"nombre": "Audífonos", "descripcion": "Headset gamer", "precio": 120.00, "stock": 15},
-    {"nombre": "Laptop", "descripcion": "Laptop Core i5", "precio": 2500.00, "stock": 8},
-    {"nombre": "USB 32GB", "descripcion": "Memoria USB 32GB", "precio": 25.00, "stock": 50},
-    {"nombre": "Webcam", "descripcion": "Webcam HD", "precio": 80.00, "stock": 12},
-    {"nombre": "Silla Gamer", "descripcion": "Silla ergonómica", "precio": 600.00, "stock": 6}, """
 ]
 
 for p in productos:
