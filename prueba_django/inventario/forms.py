aaac588b203951fb46e93c15ff8b89b417de125f
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2025-11-03 20:37:13 -0500

    ajuste de formulario y validacion

--- prueba_django/inventario/forms.py
+++ prueba_django/inventario/forms.py
@@ -1,11 +1,20 @@
 from django import forms
 from .models import Producto
 
+
 class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
-        fields = ["nombre", "descripcion", "precio"]
+        # fields = ["nombre", "descripcion", "precio"]
+        fields = "__all__"
+        widgets = {
+            "nombre": forms.TextInput(attrs={"class": "form-control"}),
+            "descripcion": forms.Textarea(attrs={"class": "form-control", "rows": 3}),
+            "precio": forms.NumberInput(attrs={"class": "form-control"}),
+            # 'stock': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
 
+    # Validación del precio positivo
     def clean_precio(self):
         precio = self.cleaned_data.get("precio")
         if precio is not None and precio <= 0:
